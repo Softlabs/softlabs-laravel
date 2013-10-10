6d3c362f9cd91ae58f17174c2a2ba20fdc6e3765
Matthew <matthew.erskine@softlabs.co.uk>
2013-10-10 13:47:31 +0100

    Add `Cur` package
    Provides an interface for currency methods and helpers.

---
+++ src/Softlabs/Cur/Cur.php
@@ -0,0 +1,26 @@
+<?php namespace Softlabs\Cur;
+
+class Cur
+{
+	/**
+	 * Converts a value to a presentable GBP format. An alternative
+	 * return value can be specified when the data provided cannot
+	 * be converted.
+	 * @param  integer $data The data to be converted to GBP format.
+	 * @param  string $returnVal A value to return if the conversion
+	 * is not possible
+	 * @return string The formatted GBP string.
+	 */
+    public function gbp($data, $returnVal='-')
+    {
+        if (is_null($data)) {
+            return $returnVal;
+        }
+
+        if ( ! is_float($data)) {
+        	$data = floatval($data);
+        }
+
+        return (is_numeric($data) ? '£' . number_format($data, 2) : $data);
+    }
+}
---
+++ src/Softlabs/Cur/CurServiceProvider.php
@@ -0,0 +1,46 @@
+<?php namespace Softlabs\Cur;
+
+use Illuminate\Support\ServiceProvider;
+
+class CurServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('softlabs/cur');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['cur'] = $this->app->share(function($app) {
+			return new Cur();
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
