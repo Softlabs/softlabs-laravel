cf64411e0667f0e0eb87c5a99e06d07b44f8e347
Matthew <matthew.erskine@softlabs.co.uk>
2013-10-25 15:29:22 +0100

    Update logic class on `Base`

--- src/Softlabs/Base/Logic.php
+++ src/Softlabs/Base/Logic.php
@@ -1,22 +1,45 @@
 <?php namespace Softlabs\Base;
 
 use Repository;
+use Illuminate\Support\Collection;
 
 abstract class Logic
 {
 	/**
-	 * The repository used to provide data to work logic on.
-	 * @var \Softlabs\Base\Repository
+	 * The repository collection used to provide data to work logic on.
+	 * @var Illuminate\Support\Collection
 	 */
-	protected $repository;
+	protected $repositoryCollection;
 
 	/**
 	 * Called when the logical class should construct itself.
-	 * @param \Softlabs\Base\Repository $repoistory The repository
+	 * @param Illuminate\Support\Collection $repositoryCollection
+	 * A collection of repositories to work logic on.
 	 * used to provide data work logic on.
 	 */
-	public function __construct(Repository $repository)
+	public function __construct(Collection $repositoryCollection)
 	{
-		$this->repository = $repository;
+		foreach ($repositoryCollection as $repository) {
+			if ( ! ($repository instanceof Repository)) {
+				throw new InvalidArgumentException(
+					'The collection specified must contain only repositories.'
+				);
+			}
+		}
+
+		$this->repositoryCollection = $repositoryCollection;
+	}
+
+	/**
+	 * Called when unknown properties are attempted to be accessed in the
+	 * logic class.
+	 * @param  string $key The property being attempted to index.
+	 * @return mixed Attempt to index property in repository collection.
+	 */
+	public function __get($key)
+	{
+		if ( isset($repositoryCollection[$key])) {
+			return $repositoryCollection[$key];
+		}
 	}
 }
