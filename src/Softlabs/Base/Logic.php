87d667df75e25dd7bf60efd33bc12ade05878540
Matthew <matthew.erskine@softlabs.co.uk>
2013-11-01 09:49:26 +0000

    Change `Base` logic back to use a single `Repository` instead of collection

--- src/Softlabs/Base/Logic.php
+++ src/Softlabs/Base/Logic.php
@@ -1,6 +1,6 @@
 <?php namespace Softlabs\Base;
 
-use Repository;
+use Softlabs\Base\Repository;
 use Illuminate\Support\Collection;
 use Illuminate\Foundation\Application as App;
 use Illuminate\Support\Facades\Facade;
@@ -8,71 +8,18 @@ use Illuminate\Support\Facades\Facade;
 abstract class Logic
 {
 	/**
-	 * The repository collection used to provide data to work logic on.
-	 * @var Illuminate\Support\Collection
+	 * The repository used to provide data to work logic on.
+	 * @var Softlabs\Base\Repository
 	 */
-	protected $repositories;
+	protected $repository;
 
 	/**
 	 * Called when the logical class should construct itself.
-	 * @param Illuminate\Support\Collection $repositories
-     * @param Illuminate\Foundation\Application $app
-	 * A collection of repositories to work logic on.
-	 * used to provide data work logic on.
+	 * @param Softlabs\Base\Repository $repository
+	 * The repository used to provide data to work logic on.
 	 */
-	public function __construct(Collection $repositories)
+	public function __construct(Repository $repository)
 	{
-		foreach ($repositories as $repository) {
-			if ( ! ($repository instanceof Repository)) {
-				throw new \InvalidArgumentException(
-					'The collection specified must contain only repositories.'
-				);
-			}
-		}
-
-		$this->repositories = $repositories;
-	}
-
-	/**
-	 * Adds a new repository to the collection.
-	 * @param string $name The class name of the repository to create.
-	 */
-	public function addRepository($name)
-	{
-
-        $instance = \App::make($name);
-
-		$this->repositories[$name] = \App::make($name);
-
-		if ( ! ($instance instanceof Repository)) {
-			throw new \InvalidArgumentException(
-				"The class [$name] is not a valid repository."
-			);
-		}
-
-		$this->repositories[$name] = $instance;
-	}
-
-	/**
-	 * Retrieves a repository from the collection.
-	 * @param  string $name The class name of the repository to retrieve.
-	 * @return Repository Repository instance
-	 */
-	public function getRepository($name)
-	{
-		return $this->repositories[$name];
-	}
-
-	/**
-	 * Called when unknown properties are attempted to be accessed in the
-	 * logic class.
-	 * @param  string $key The property being attempted to index.
-	 * @return mixed Attempt to index property in repository collection.
-	 */
-	public function __get($key)
-	{
-		if ( isset($repositories[$key])) {
-			return $repositories[$key];
-		}
+		$this->repository = $repository;
 	}
 }
